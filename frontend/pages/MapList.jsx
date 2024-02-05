ab3179c094cbb0cde62221684cf0577cbd0bf261
Maddie Weber <maddieweber7@gmail.com>
2024-02-05 16:45:03 -0500

    working on CSS

--- frontend/pages/MapList.jsx
+++ frontend/pages/MapList.jsx
@@ -129,19 +129,22 @@ const MapList = () => {
     return (
         <div>
             <Header/>
-            <form id="party-size-form">
-                <label htmlFor="party-size">Party Size (&lt;=10): </label>
-                <input 
-                    type="number" 
-                    id="party-size" 
-                    name="party-size" 
-                    min="1" 
-                    value={partySize} 
-                    onChange={handlePartySizeChange} 
-                    required 
-                />
-                <button id="getLocationBtn" type="button" onClick={handleGetLocation}>See Restaurants with Availability</button>
-            </form>
+            <div class="form-container">
+                <form id="party-size-form">
+                    <label htmlFor="party-size">Enter Party Size</label> 
+                    <label htmlFor="party-size">(&lt;=10)</label>
+                    <input 
+                        type="number" 
+                        id="party-size" 
+                        name="party-size" 
+                        min="1" 
+                        value={partySize} 
+                        onChange={handlePartySizeChange} 
+                        required 
+                    />
+                    <button id="getLocationBtn" type="button" onClick={handleGetLocation}>See Restaurants</button>
+                </form>
+            </div>
 
             {loading ? ( // Conditionally render loading message
                 <p>Loading...</p>
