e47a6bcd2dcd2fc4e8e0805bd1f5c817fffad623
Maddie Weber <maddieweber7@gmail.com>
2024-02-05 17:16:49 -0500

    CSS looking decent

--- frontend/pages/LoginForm.jsx
+++ frontend/pages/LoginForm.jsx
@@ -32,29 +32,30 @@ function LoginForm() {
     }
 
     return (
-        <div className="page-container"> {/* Apply container style to center content */}
+        <div>
             <Header />
+            <div className="page-container"> {/* Apply container style to center content */}
             <div className="form-container"> {/* Apply form container style */}
                 <form id='login-form' onSubmit={handleLoginSubmit}>
-                    <label className='login-text'>Username:</label>
                     <input
                         type='text'
-                        placeholder="Enter your username"
+                        placeholder="Username"
                         className='login-input'
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
-                    <label className='login-text'>Password:</label>
                     <input
                         type='password'
-                        placeholder="Enter your password"
+                        placeholder="Password"
                         className='login-input'
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    <button id='login-submit-button' type='submit'>Submit</button>
+                    <button id='login-submit-button' type='submit'>Log In</button>
                 </form>
             </div>
+        </div>
+
         </div>
     );
 };
