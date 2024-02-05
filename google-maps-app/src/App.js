ab97eaf2c9e4a37badc7d1290aa8a039c2c1b8b0
Maddie Weber <maddieweber7@gmail.com>
2024-02-05 10:52:28 -0500

    trying to run google maps but not really working

---
+++ google-maps-app/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat: 7.2905715, // default latitude
+  lng: 80.6337262, // default longitude
+};
+
+const App = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyBvOI7WrS042mwH-Gj3vDZG0UP6wtKW5ZU',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default App;
